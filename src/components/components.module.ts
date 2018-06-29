7b5fc63906f51147d16ba8ad6dda7e613ceb52dd
Bas Hoogzaad <bashoogzaad@gmail.com>
2018-06-29 15:51:24 +0200

    busy

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonFooterComponent } from './common-footer/common-footer';
-import { ProgressBarModule } from "angular-progress-bar";
+import { ProgressBarComponent } from './progress-bar/progress-bar';
 
 @NgModule({
-	declarations: [CommonFooterComponent],
-	imports: [ProgressBarModule],
-	exports: [CommonFooterComponent]
+	declarations: [CommonFooterComponent,
+    ProgressBarComponent],
+	imports: [],
+	exports: [CommonFooterComponent,
+    ProgressBarComponent]
 })
 export class ComponentsModule {}
